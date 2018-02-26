b34e4144d38752847719d695acf5912c049a22c5
sriramram1995 <sriramram1995@gmail.com>
2018-02-26 14:28:55 +0000

    assignment3

--- config/auth.js
+++ config/auth.js
@@ -2,8 +2,8 @@
 module.exports = {
 
   'facebookAuth' : {
-      'clientID'      : '1648221025215090', // your App ID
-      'clientSecret'  : '1ab0cb25035a37e4bf93b334c2691904 ',
+      'clientID'      : '152259558812236', // your App ID
+      'clientSecret'  : '4f77a5a344537cea9474ee4e2f7ad7ac',
       'callbackURL'   : 'http://ukiassignment3byram.herokuapp.com/_oauth/facebook',
       'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
       'profileFields' : ['id', 'emails', 'name'] // For requesting permissions from Facebook API
